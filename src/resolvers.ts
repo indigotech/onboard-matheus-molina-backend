71f4e200f23e2697c3039b97d20d081e6746a4ab
MaTMolina <matheus_molina@usp.br>
2022-05-03 12:53:09 -0300

    create dumb mutation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -4,4 +4,14 @@ export const resolvers = {
   Query: {
     books: () => books,
   },
+  Mutation: {
+    createUser: (data: any) => MOCK_USER,
+  },
+};
+
+export const MOCK_USER = {
+  id: 1,
+  name: "Joshua",
+  email: "User e-mail",
+  birthDate: "01-01-1990",
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,6 @@
 import { gql } from "apollo-server";
 
 export const typeDefs = gql`
-
   type Book {
     title: String
     author: String
@@ -9,5 +8,23 @@ export const typeDefs = gql`
 
   type Query {
     books: [Book]
+  }
+
+  input UserInput {
+    email: String
+    name: String
+    birthDate: String
+    password: String
+  }
+
+  type User {
+    id: Int
+    email: String
+    name: String
+    birthDate: String
+  }
+
+  type Mutation {
+    createUser(data: UserInput!): User
   }
 `;
