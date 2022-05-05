efa2845a903ee89acfd828c8eb443df0fe2023c2
MaTMolina <matheus_molina@usp.br>
2022-05-05 17:19:47 -0300

    expect for evaluating tests

--- e2e/index.ts
+++ e2e/index.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
+import { expect } from "chai";
 import * as dotenv from "dotenv";
 import { before, describe, it } from "mocha";
+import { books } from "../src/database";
 import { setup } from "../src/setup";
 
 describe("Test", () => {
   before(async () => {
-    console.log("entering before");
     dotenv.config({ path: __dirname + "/../test.env" });
     await setup();
   });
 
-  it("Should create User", async () => {
+  it("Should get books", async () => {
     const response = await axios.post("http://localhost:4001/graphql", {
       operationName: null,
       variables: {},
@@ -23,6 +24,6 @@ describe("Test", () => {
           }
         `,
     });
-    console.log(response.data);
+    expect(response.data.data.books).to.be.deep.eq(books);
   });
 });
