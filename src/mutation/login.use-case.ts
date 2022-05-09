b196806c5304e12d525665e5c896316a0009b10b
MaTMolina <matheus_molina@usp.br>
2022-05-09 17:01:16 -0300

    add errors

--- src/mutation/login.use-case.ts
+++ src/mutation/login.use-case.ts
@@ -1,6 +1,9 @@
 import * as crypto from "crypto";
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
+import { CustomError } from "../errors/login-error-class";
+import { validatePassword } from "../validators/password-validator";
+import { CreateUserInput } from "./create-user.use-case";
 
 interface LoginInput {
   email: string;
@@ -10,14 +13,28 @@ export async function login(input: LoginInput) {
   const registeredEmailUser = await AppDataSource.manager.findOneBy(User, {
     email: input.email,
   });
-  const { salt } = registeredEmailUser!;
+  const isValidPassword = validatePassword(input.password);
 
+  if (!isValidPassword) {
+    throw new CustomError(
+      400,
+      "The password must contain at least 6 characters, of which 1 must be a letter and 1 a digit"
+    );
+  }
+
+  if (!registeredEmailUser) {
+    throw new CustomError(400, "Email not found");
+  }
+
+  const { salt } = registeredEmailUser!;
   const hash = crypto.createHash("sha256");
   hash.update(salt + input.password);
   const hashedPassword = hash.digest("hex");
 
   if (hashedPassword === registeredEmailUser?.password) {
     return { user: registeredEmailUser, token: "" };
+  } else {
+    throw new CustomError(401, "Wrong Email and/or Paassword");
   }
 }
 
