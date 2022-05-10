0994fdb47da6f523756acfcc8c490b8f0a411d5a
MaTMolina <matheus_molina@usp.br>
2022-05-10 15:32:45 -0300

    create query

---
+++ src/query/get-user.query.ts
@@ -0,0 +1,17 @@
+import { verifyToken } from "../cryptography/verify-token";
+import { AppDataSource } from "../data-source";
+import { User } from "../entity/User";
+import { CustomError } from "../errors/login-error-class";
+import { isAuthorized } from "../validators/authorization-validator";
+
+export async function getUser(id: number, token: string) {
+  const decoded = await verifyToken(token, "secretKey");
+  const authorized = await isAuthorized(decoded!.id);
+
+  if (!authorized) {
+    throw new CustomError(401, "User Unauthorized, please create User");
+  }
+
+  const requestedUser = await AppDataSource.manager.findOneBy(User, { id: id });
+  return requestedUser;
+}
